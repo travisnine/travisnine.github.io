e60ce22fc248e9ed1bcd178f08e04be546c010e9
travisnine-laptop <tj@travis9.com>
2015-07-03 11:07:00 -0400

    refactor

--- feedly-to-tumblr/main.js
+++ feedly-to-tumblr/main.js
@@ -3,29 +3,33 @@ javascript:(function(){ var scriptLoc = '//travisnine.github.io/feedly-to-tumblr
 */
 
 //vars
-window.f2t = {};
-window.f2t.logElement = {innerHTML:'',innerText:''};
+function loadScript() {
+	if (window.f2t) return false;
+	
+	window.f2t = {};
+	window.f2t.logElement = {innerHTML:'',innerText:''};
 
-//funcs
-window.f2t.log = 
-	function(sTextToLog) {
-		window.f2t.logElement.innerText = sTextToLog;
-	};
-window.f2t.keyupHandler =
-	function(keyEvt) {
-		// TODO: Sort through list with keys
-	};
-window.f2t.init = 
-	function() {
-		var newDiv = window.document.createElement('div');
-		newDiv.innerHTML = ''
-			+ '<div id="f2tDiv" style="padding:10px;z-index:1000000;position:fixed;background-color:wheat;width:100%;">'
-			+ 'Hello from f2t!'
-			+ '</div>'
-		;
-		window.document.body.insertBefore(newDiv,window.document.body.firstChild);
-		window.f2t.logElement = document.getElementById('f2tDiv');
-		window.addEventListener('keyup',window.f2t.keyupHandler,true);
-	};
+	//funcs
+	window.f2t.log = 
+		function(sTextToLog) {
+			window.f2t.logElement.innerText = sTextToLog;
+		};
+	window.f2t.keyupHandler =
+		function(keyEvt) {
+			// TODO: Sort through list with keys
+		};
+	window.f2t.init = 
+		function() {
+			var newDiv = window.document.createElement('div');
+			newDiv.innerHTML = ''
+				+ '<div id="f2tDiv" style="padding:10px;z-index:1000000;position:fixed;background-color:wheat;width:100%;">'
+				+ 'Hello from f2t!'
+				+ '</div>'
+			;
+			window.document.body.insertBefore(newDiv,window.document.body.firstChild);
+			window.f2t.logElement = document.getElementById('f2tDiv');
+			window.addEventListener('keyup',window.f2t.keyupHandler,true);
+		};
 
-window.f2t.init();
+	window.f2t.init();
+}();
