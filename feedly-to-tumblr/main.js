8c2d8a84985349a28c2675880d739cb5b3547d1d
travisnine-laptop <tj@travis9.com>
2015-07-03 04:29:40 -0400

    work

--- feedly-to-tumblr/main.js
+++ feedly-to-tumblr/main.js
@@ -1,17 +1,27 @@
 /* bookmarklet = 
 javascript:(function(){ var scriptLoc = '//travisnine.github.io/feedly-to-tumblr/main.js'; var scriptEl = document.createElement('script'); scriptEl.src = scriptLoc; document.body.appendChild(scriptEl); })();
 */
-
 window.f2t = {};
+window.f2t.logElement = {innerHTML:'',innerText:''};
+window.f2t.log = 
+	function(sTextToLog) {
+		window.f2t.logElement.innerText = sTextToLog;
+	};
+window.f2t.keyupHandler =
+	function() {
+		console.log('f2t: key up event');
+	};
 window.f2t.init = 
 	function() {
 		var newDiv = window.document.createElement('div');
 		newDiv.innerHTML = ''
-			+ '<div id="f2tDiv" style="padding: 10px;z-index: 99999;position: fixed;background-color: wheat;width: 100%;">'
+			+ '<div id="f2tDiv" style="padding:10px;z-index:1000000;position:fixed;background-color:wheat;width:100%;">'
 			+ 'Hello from f2t!'
 			+ '</div>'
 		;
 		window.document.body.insertBefore(newDiv,window.document.body.firstChild);
+		window.f2t.logElement = document.getElementById('f2tDiv');
+		window.addEventListener('keyup',window.f2t.keyupHandler,true);
 	};
 
 window.f2t.init();
