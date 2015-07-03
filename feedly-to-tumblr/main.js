b338a52ba3096e442e4258cda3058835f3507084
travisnine-laptop <tj@travis9.com>
2015-07-03 04:18:09 -0400

    work

--- feedly-to-tumblr/main.js
+++ feedly-to-tumblr/main.js
@@ -1,11 +1,17 @@
+/* bookmarklet = 
+javascript:(function(){ var scriptLoc = '//travisnine.github.io/feedly-to-tumblr/main.js'; var scriptEl = document.createElement('script'); scriptEl.src = scriptLoc; document.body.appendChild(scriptEl); })();
+*/
+
 window.f2t = {};
 window.f2t.init = 
 	function() {
 		var newDiv = window.document.createElement('div');
-		var newDiv.innerHTML = ''
-			+ '<div id="f2tDiv" style="padding: 10px;">'
+		newDiv.innerHTML = ''
+			+ '<div id="f2tDiv" style="padding: 10px;z-index: 99999;position: fixed;background-color: wheat;width: 100%;">'
 			+ 'Hello from f2t!'
 			+ '</div>'
 		;
-		window.document.insertBefore(newDiv,window.document.firstChild);
+		window.document.body.insertBefore(newDiv,window.document.body.firstChild);
 	};
+
+window.f2t.init();
